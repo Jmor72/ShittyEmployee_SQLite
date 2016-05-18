72ece41d6182b6090a8d01b17cad39e551be3d12
Jmor72 <jaren.morris@gmail.com>
2016-05-17 21:41:28 -0600

    ShittyEmployee_SQLite wtf cant connect

---
+++ src/ShittyEmployee_SQLite/Controllers/EmployeeController.cs
@@ -0,0 +1,51 @@
+﻿using ShittyEmployee_SQLite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using System.Net;
+using System.Web.Http;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ShittyEmployee_SQLite.Controllers
+{
+    [Route("api/[controller]")]
+    public class EmployeeController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            //SELECT SQLite Cmd goes here
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+            //INSERT SQLite Cmd goes here
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ src/ShittyEmployee_SQLite/Database.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.SQLite;
+
+namespace ShittyEmployee_SQLite
+{
+    class Database
+    {
+        public SQLiteConnection sqlite_conn;
+        public SQLiteCommand sqlite_cmd;
+        public SQLiteDataReader sqlite_datareader;
+
+        public Database()
+        {
+            sqlite_conn = new SQLiteConnection("Data Source=EmployeeDB.sqlite3");
+            sqlite_conn.Open();
+            sqlite_cmd = sqlite_conn.CreateCommand();
+            //SQLiteConnection m_dbConnection = new SQLiteConnection("Data Source=EmployeeDB.sqlite3;version=3.8.8.3;");
+        }
+    }
+}
